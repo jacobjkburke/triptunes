bdce3865de1fbad79502f77118a309418e71e800
Liam Brozik <lbrozik@uw.edu>
2017-11-27 19:53:18 -0800

    Adjusted MDL styling, added Places Autocomplete to text input on main card

---
+++ src/app.js
@@ -0,0 +1,4 @@
+var start = document.getElementById("start");
+var autocompleteStart = new google.maps.places.Autocomplete(start);
+var destination = document.getElementById("destination");
+var autocompleteStart = new google.maps.places.Autocomplete(destination);
