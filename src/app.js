3f6a1d8a5c0452101a99cf2382073c7aade2ef5e
Jacob Burke <jacobburke@D-173-250-181-212.dhcp4.washington.edu>
2017-11-30 16:16:07 -0800

    grabbed inputed values

--- src/app.js
+++ src/app.js
@@ -2,3 +2,27 @@ var start = document.getElementById("start");
 var autocompleteStart = new google.maps.places.Autocomplete(start);
 var destination = document.getElementById("destination");
 var autocompleteStart = new google.maps.places.Autocomplete(destination);
+var genre = document.getElementById("genre");
+//auto complete for song genre maybe?
+var methodRadios = document.getElementsByName("options");
+var methodArray = ["driving", "walking", "bicycling"];
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", submitFunction);
+
+function submitFunction() {
+    for (var i = 0, length = methodRadios.length; i < length; i++) {
+        if (methodRadios[i].checked) {
+            // do whatever you want with the checked radio
+            var travelMethod = methodArray[i];
+            console.log(travelMethod);
+            break;
+        }
+    }
+    var startValue = start.value;
+    console.log(startValue);
+    var destinationValue = destination.value;
+    console.log(destinationValue);
+    var genreValue = genre.value;
+    console.log(genreValue);
+}
+
