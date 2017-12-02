d337ab31d28ed5eaa1e972830c328cbe6c6e0088
Liam Brozik <lbrozik@uw.edu>
2017-12-01 20:20:28 -0800

    Merging code_files into master

--- src/app.js
+++
@@ -1,4 +0,0 @@
-var start = document.getElementById("start");
-var autocompleteStart = new google.maps.places.Autocomplete(start);
-var destination = document.getElementById("destination");
-var autocompleteStart = new google.maps.places.Autocomplete(destination);
